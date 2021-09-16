df2386d3c9d401e3e03d977340dc97f55bffacf2
Anastasija <ernistaill@gmail.com>
2021-09-16 10:25:04 +0300

    create table

--- src/main/java/com/github/ernysent/yggdrasil/domain/Worker.java
+++ src/main/java/com/github/ernysent/yggdrasil/domain/Worker.java
@@ -14,12 +14,13 @@ public class Worker {
 
     }
 
-    public Worker(int id, String firstName, String lastName,
+    public Worker(int id, String firstName, String lastName, String position,
                   String phoneNumber){
         super();
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
+        this.position = position;
         this.phoneNumber = phoneNumber;
 
     }
@@ -30,5 +31,36 @@ public class Worker {
     public void setId(int id){
         this.id = id;
     }
+    public String getFirstName(){
+        return firstName;
+    }
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+    public String getLastName(){
+        return lastName;
+    }
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+    public String getPosition(){
+        return position;
+    }
+    public void setPosition(String position){
+        this.position = position;
+    }
+    public String getActive(){
+        return active;
+    }
+    public void setActive(String active){
+        this.active = active;
+    }
+    public String getPhoneNumber(){
+        return phoneNumber;
+    }
+    public  void setPhoneNumber(String phoneNumber){
+        this.phoneNumber = phoneNumber;
+    }
+
 }
 
