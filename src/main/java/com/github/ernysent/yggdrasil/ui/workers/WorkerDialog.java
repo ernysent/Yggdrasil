9d36baa529600bc1e45a4d6f5278d66e9d0aa032
ernysent <ernistaill@gmail.com>
2021-09-21 12:18:37 +0300

    2021-09-21

---
+++ src/main/java/com/github/ernysent/yggdrasil/ui/workers/WorkerDialog.java
@@ -0,0 +1,96 @@
+package com.github.ernysent.yggdrasil.ui.workers;
+
+import com.github.ernysent.yggdrasil.data.WorkerRepository;
+import com.github.ernysent.yggdrasil.domain.Worker;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.checkbox.Checkbox;
+import com.vaadin.flow.component.combobox.ComboBox;
+import com.vaadin.flow.component.dialog.Dialog;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import org.springframework.stereotype.Component;
+
+@Component
+public class WorkerDialog extends Dialog {
+    private TextField nameField = new TextField("First Name");
+    private TextField lastNameField = new TextField("Last Name");
+    private TextField phoneField = new TextField("Phone number");
+    private ComboBox<String> positionField = new ComboBox<>();
+    private Checkbox activeField = new Checkbox();
+    private Worker worker;
+
+    public void setSetWorker(Worker worker) {
+        this.worker = worker;
+        if (worker != null) {
+            nameField.setValue(worker.getFirstName());
+            lastNameField.setValue(worker.getLastName());
+            phoneField.setValue(worker.getPhoneNumber());
+            positionField.setValue(worker.getPosition());
+            activeField.setValue(worker.getActive());
+        } else {
+            nameField.clear();
+            lastNameField.clear();
+            phoneField.clear();
+            positionField.clear();
+            activeField.clear();
+        }
+    }
+
+    public interface ChangeHandler {
+        void onChange();
+    }
+
+    private ChangeHandler changeHandler;
+
+    public void setChangeHandler(ChangeHandler changeHandler) {
+        this.changeHandler = changeHandler;
+    }
+
+    public WorkerDialog(WorkerRepository workerRepository){
+
+        // Design
+
+
+        positionField.setItems("Painter","Assembler");
+        positionField.setLabel("Position");
+        positionField.setPlaceholder("Position");
+
+
+        activeField.setLabel("Active");
+        activeField.setValue(true);
+
+        Button closeButton = new Button("Close");
+        Button saveButton = new Button("Save");
+        HorizontalLayout fieldsLayout = new HorizontalLayout(nameField,lastNameField,positionField,phoneField,activeField);
+        fieldsLayout.setSpacing(true);
+        HorizontalLayout buttonLayout = new HorizontalLayout(closeButton,saveButton);
+        //buttonLayout.setSpacing();
+        add(fieldsLayout, buttonLayout);
+
+        // Events
+        closeButton.addClickListener(click -> {
+            close();
+        });
+
+        saveButton.addClickListener(click -> {
+
+            if(worker == null) {
+                worker = new Worker();
+            }
+            worker.setFirstName(nameField.getValue());
+            worker.setLastName(lastNameField.getValue());
+            worker.setPosition(positionField.getValue());
+            worker.setPhoneNumber(phoneField.getValue());
+            worker.setActive(activeField.getValue());
+
+
+            workerRepository.save(worker);
+
+            changeHandler.onChange();
+            //grid.getDataProvider().refreshAll();
+
+            close();
+
+        });
+    }
+}
--- src/main/java/com/github/ernysent/yggdrasil/ui/workers/WorkersLayout.java
+++ src/main/java/com/github/ernysent/yggdrasil/ui/workers/WorkersLayout.java
@@ -3,13 +3,10 @@ package com.github.ernysent.yggdrasil.ui.workers;
 import com.github.ernysent.yggdrasil.data.WorkerRepository;
 import com.github.ernysent.yggdrasil.domain.Worker;
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.checkbox.Checkbox;
-import com.vaadin.flow.component.combobox.ComboBox;
-import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PreserveOnRefresh;
 import com.vaadin.flow.spring.annotation.UIScope;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,74 +21,33 @@ import java.util.List;
 public class WorkersLayout extends VerticalLayout {
 
     @Autowired
-    public WorkersLayout(WorkerRepository workerRepository) {
+    public WorkersLayout(
+            WorkerRepository workerRepository,
+            WorkerDialog workerDialog
+    ) {
         List<Worker> workersList = new ArrayList<>();
         Grid<Worker> grid = new Grid<>(Worker.class);
         grid.setItems(workersList);
-        Dialog dialog = new Dialog();
 
-        Button addWorkerButton = new Button("Add", event ->{
-            TextField nameField = new TextField("First Name");
-            TextField lastNameField = new TextField("Last Name");
-            TextField phoneField = new TextField("Phone number");
-
-            ComboBox<String> positionField = new ComboBox<>();
-            positionField.setItems("Painter","Assembler");
-            positionField.setLabel("Position");
-            positionField.setPlaceholder("Position");
-
-            Checkbox activeField = new Checkbox();
-            activeField.setLabel("Active");
-            activeField.setValue(true);
-            add(activeField);
-
-//            ComboBox<Boolean> activeField = new ComboBox();
-//            activeField.setLabel("Active");
-//            activeField.setItems(true,false);
-//            activeField.setValue(false);
-
-
-//            TextField positionField = new TextField("Position");
-
-            Button closeButton = new Button("Close");
-            Button saveButton = new Button("Save");
-
-
-            closeButton.addClickListener(click -> {
-                dialog.close();
-            });
-
-            saveButton.addClickListener(click -> {
-                Worker worker = new Worker();
-                worker.setFirstName(nameField.getValue());
-                worker.setLastName(lastNameField.getValue());
-                worker.setPosition(positionField.getValue());
-                worker.setPhoneNumber(phoneField.getValue());
-                worker.setActive(activeField.getValue());
-
-
-                workerRepository.save(worker);
-                workersList.clear();
-                workerRepository.findAll().forEach(workersList::add);
-                grid.setItems(workersList);
-                //grid.getDataProvider().refreshAll();
-                dialog.close();
-            });
-
-            HorizontalLayout fieldsLayout = new HorizontalLayout(nameField,lastNameField,positionField,phoneField,activeField);
-            fieldsLayout.setSpacing(true);
-            HorizontalLayout buttonLayout = new HorizontalLayout(closeButton,saveButton);
-            //buttonLayout.setSpacing();
-            dialog.add(fieldsLayout, buttonLayout);
-
-            dialog.open();
-
+        Button addWorkerButton = new Button("Add", event ->
+        {
+                workerDialog.setSetWorker(null);
+                workerDialog.open();
         });
 
-        Button removeButton = new Button("Remove last", event ->{
-           workersList.remove(workersList.size()-1);
-//           workerRepository.deleteById();
-           grid.getDataProvider().refreshAll();
+        Button removeButton = new Button("Remove");
+        removeButton.addClickListener( click ->{
+            Worker worker = grid.asSingleSelect().getValue();
+            if (worker != null){
+                workerRepository.delete(worker);
+                workerRepository.findAll().forEach(workersList::add);
+                grid.setItems(workersList);
+                //workersList.remove(worker);
+                //grid.getDataProvider().refreshAll();
+
+            }else { Notification.show("Please select Worker");};
+
+
         });
 
 //        Edit Button
@@ -100,10 +56,13 @@ public class WorkersLayout extends VerticalLayout {
 
         Button editButton = new Button("Edit");
         editButton.addClickListener(click ->{
-            grid.asSingleSelect().getValue();
-
-            dialog.open();
-
+            Worker worker = grid.asSingleSelect().getValue();
+            if (worker != null) {
+                workerDialog.setSetWorker(worker);
+                workerDialog.open();
+            } else {
+                Notification.show("Please select Worker");
+            }
         });
 
         // Get Workers from DB and place in List
@@ -125,7 +84,12 @@ public class WorkersLayout extends VerticalLayout {
         HorizontalLayout remeditButton = new HorizontalLayout(removeButton,editButton);
         add(addWorkerButton, remeditButton, grid);
 
-
+        // Events
+        workerDialog.setChangeHandler(() -> {
+            workersList.clear();
+            workerRepository.findAll().forEach(workersList::add);
+            grid.setItems(workersList);
+        });
 //
     }
 
