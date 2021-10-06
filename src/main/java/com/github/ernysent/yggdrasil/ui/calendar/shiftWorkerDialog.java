d222ef5e07e1ad627dda62ccc097e8e22ec3b947
ernysent <ernistaill@gmail.com>
2021-10-06 12:16:44 +0300

    2021-10-06 sheftsDialog + Dialog

--- src/main/java/com/github/ernysent/yggdrasil/ui/calendar/shiftWorkerDialog.java
+++ src/main/java/com/github/ernysent/yggdrasil/ui/calendar/shiftWorkerDialog.java
@@ -8,6 +8,8 @@ import com.vaadin.flow.component.Unit;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.spring.annotation.VaadinSessionScope;
 import java.util.ArrayList;
 import java.util.List;
@@ -22,6 +24,8 @@ public class shiftWorkerDialog extends Dialog {
   private Button addWorkersButton = new Button("Add");
   private Button saveButton = new Button("Save");
   private Button closeButton = new Button("Close");
+  private Label search = new Label("Search");
+  private Label label2 = new Label("Workers active list");
 
 
   private Grid<Worker> gridActiveWorkers = new Grid<>(Worker.class);
@@ -32,11 +36,23 @@ public class shiftWorkerDialog extends Dialog {
     List<Worker> workerList = new ArrayList<>();
     gridActiveWorkers.setItems(workerList);
 
+
+
+
+
+
     gridActiveWorkers.setColumns("firstName","lastName","phoneNumber","position");
 //    gridActiveWorkers.asSingleSelect().setValue(workerList.get(0));
     gridActiveWorkers.setItems(workerService.findAll());
 
-    add(gridActiveWorkers);
+
+    closeButton.addClickListener(click ->{
+      close();
+    });
+
+    HorizontalLayout searchLayout = new HorizontalLayout(addSearchWorkerButton);
+    HorizontalLayout buttonLayout = new HorizontalLayout(addWorkersButton,closeButton);
+    add(search,searchLayout,label2,gridActiveWorkers,buttonLayout);
 
   }
 
